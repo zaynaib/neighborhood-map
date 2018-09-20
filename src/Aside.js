eed64701278db66cac5c63ae00eee234c2409d08
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-20 14:09:09 -0500

    style and feat: created header and style header

---
+++ src/Aside.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Aside extends Component {
+    render() {
+      return (
+        <nav>
+            
+        </nav>
+      );
+    }
+  }
+  
+  export default Aside;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends Component {
+    render() {
+      return (
+        <header>
+            <h1>Neighbor Hood Map</h1>
+        </header>
+      );
+    }
+  }
+  
+  export default Header;
+  
